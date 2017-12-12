59db481e490bdd0ae7e02d350647dd5d8fc8f7a9
averrin <averrin@gmail.com>
2017-12-12 23:56:24 +0300

    mc

--- src/GeneratorFacade.cpp
+++ src/GeneratorFacade.cpp
@@ -19,6 +19,11 @@ extern "C" {
 
 		world = json({});
 		auto regions = json::array();
+		auto mClusters = json::array();
+		for (auto c : mapgen->map->megaClusters) {
+			mClusters.push_back(c->name);
+		}
+
 		int i = 0;
 		for (auto r : mapgen->map->regions) {
 			auto json_r = json({});
@@ -32,13 +37,18 @@ extern "C" {
 				f_points.push_back({ {"x", p->x}, {"y", p->y}, {"height", r->getHeight(p)} });
 			}
 			json_r["points"] = f_points;
+
+			json_r["site"] = { {"x", r->site->x}, {"y", r->site->y}, {"height", r->getHeight(r->site)} };
 			json_r["biom"] = r->biom.name;
 			json_r["isLand"] = r->megaCluster->isLand;
+			auto it = std::find(mapgen->map->megaClusters.begin(), mapgen->map->megaClusters.end(), r->megaCluster);
+			json_r["megaCluster"] = std::distance(mapgen->map->megaClusters.begin(), it);
 			regions.push_back(json_r);
 			i++;
 		}
 
 		world["regions"] = regions;
+		world["megaClusters"] = mClusters;
 		auto dump = world.dump();
 		std::strcpy(buff, dump.c_str());
 		return dump.length() + 1;
