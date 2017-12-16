e87026310bdbfa3a8aa019e24dd08d5c2db73f5e
averrin <averrin@gmail.com>
2017-12-16 17:29:37 +0300

    regions in output. Its for combined meshes

--- src/GeneratorFacade.cpp
+++ src/GeneratorFacade.cpp
@@ -20,9 +20,40 @@ extern "C" {
 		world = json({});
 		auto regions = json::array();
 		auto mClusters = json::array();
-		for (auto c : mapgen->map->megaClusters) {
-			mClusters.push_back(c->name);
+		auto clusters = json::array();
+		int n = 0;
+		for (auto c : mapgen->map->clusters) {
+			auto mc = json({});
+			mc["id"] = n;
+			mc["name"] = c->name;
+			mc["biom"] = c->regions.front()->biom.name;
+			mc["regions"] = c->regions.size();
+			clusters.push_back(mc);
+			n++;
 		}
+
+		n = 0;
+		for (auto c : mapgen->map->megaClusters) {
+			auto mc = json({});
+			mc["id"] = n;
+			mc["name"] = c->name;
+			mc["clusters"] = json::array();
+			for (auto r : c->regions)
+			{
+				auto it = std::find(mapgen->map->clusters.begin(), mapgen->map->clusters.end(), r->cluster);
+				mc["clusters"].push_back(std::distance(mapgen->map->clusters.begin(), it));
+			}
+
+			auto v = mc["clusters"];
+			std::sort(v.begin(), v.end());
+			auto last = std::unique(v.begin(), v.end());
+			v.erase(last, v.end());
+			mc["clusters"] = v;
+			mc["regions"] = c->regions.size();
+			mClusters.push_back(mc);
+			n++;
+		}
+
 
 		int i = 0;
 		for (auto r : mapgen->map->regions) {
@@ -43,12 +74,17 @@ extern "C" {
 			json_r["isLand"] = r->megaCluster->isLand;
 			auto it = std::find(mapgen->map->megaClusters.begin(), mapgen->map->megaClusters.end(), r->megaCluster);
 			json_r["megaCluster"] = std::distance(mapgen->map->megaClusters.begin(), it);
+
+			it = std::find(mapgen->map->clusters.begin(), mapgen->map->clusters.end(), r->cluster);
+			json_r["cluster"] = std::distance(mapgen->map->clusters.begin(), it);
+
 			regions.push_back(json_r);
 			i++;
 		}
 
 		world["regions"] = regions;
 		world["megaClusters"] = mClusters;
+		world["clusters"] = clusters;
 		auto dump = world.dump();
 		std::strcpy(buff, dump.c_str());
 		return dump.length() + 1;
